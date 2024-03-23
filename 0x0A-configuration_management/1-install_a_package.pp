a52c7635b180d029a7ca9562d4576cce4af668ac
DaBetterman <1ebrahimr@gmail.com>
2024-03-23 12:05:50 +0200

    update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
+#!/usr/bin/pup
 # A puppet installing flask, ensuring it is v2.1.0
-# which is a package provider from pip3.
-# By using the ensure attribute I am making
-# sure that the set version will be installed.
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
