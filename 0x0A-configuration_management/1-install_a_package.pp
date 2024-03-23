c5057bec19f55beb5e26c9b1e601c895d3fb0f9b
DaBetterman <1ebrahimr@gmail.com>
2024-03-23 12:16:05 +0200

    update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,14 @@
 #!/usr/bin/pup
-# Puppet script to install Flask version 2.1.0
+# Puppet script to install Flask version 2.1.0 and its dependencies
 
 # Ensure pip is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
-# Install Flask using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],  # Ensure pip is installed first
+# Install Flask and its dependencies using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+  require => Package['python3-pip'],
 }
